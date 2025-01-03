65c94b33bd1b1fadcc9afa483508f9591900cc85
Julian Alvarez <juliancamiloalvarez77@gmail.com>
2025-01-03 14:36:04 -0500

    Document some funcs

--- expectations.go
+++ expectations.go
@@ -10,6 +10,9 @@ import (
 
 type ExpectFunc func(expected []byte) bool
 
+// StringMatch returns an ExpectFunc that checks if the actual byte slice matches
+// the expected string. If exact is true, it checks for an exact match. Otherwise,
+// it checks if the expected string is contained within the actual byte slice.
 func StringMatch(expected string, exact bool) ExpectFunc {
 	return func(actual []byte) bool {
 		if exact {
@@ -20,24 +23,32 @@ func StringMatch(expected string, exact bool) ExpectFunc {
 	}
 }
 
+// PrefixMatch returns an ExpectFunc that checks if the actual byte slice
+// starts with the expected string.
 func PrefixMatch(expected string) ExpectFunc {
 	return func(actual []byte) bool {
 		return strings.HasPrefix(string(actual), expected)
 	}
 }
 
+// SuffixMatch returns an ExpectFunc that checks if the actual byte slice
+// ends with the expected string.
 func SuffixMatch(expected string) ExpectFunc {
 	return func(actual []byte) bool {
 		return strings.HasSuffix(string(actual), expected)
 	}
 }
 
+// ValidUTF8 returns an ExpectFunc that checks if the actual byte slice
+// is valid UTF-8.
 func ValidUTF8() ExpectFunc {
 	return func(actual []byte) bool {
 		return utf8.Valid(actual)
 	}
 }
 
+// RegexMatch returns an ExpectFunc that checks for matches against the
+// provided regular expression pattern.
 func RegexMatch(pattern string) ExpectFunc {
 	re := regexp.MustCompile(pattern)
 	return func(actual []byte) bool {
@@ -45,12 +56,16 @@ func RegexMatch(pattern string) ExpectFunc {
 	}
 }
 
+// BytesMatch returns an ExpectFunc that checks if the actual byte slice
+// matches the expected byte slice.
 func BytesMatch(expected []byte) ExpectFunc {
 	return func(actual []byte) bool {
 		return bytes.Equal(expected, actual)
 	}
 }
 
+// RunesMatch returns an ExpectFunc that checks if the actual byte slice
+// matches the expected rune slice.
 func RunesMatch(expected []rune) ExpectFunc {
 	return func(actual []byte) bool {
 		return slices.Equal(bytes.Runes(actual), expected)
--- logtester.go
+++ logtester.go
@@ -6,6 +6,10 @@ import (
 	"sync"
 )
 
+// WTester is a wrapper around an [io.Writer] that allows
+// for expectations to be set on the output of the writer.
+// It can be used to test loggers, writers, or any other
+// [io.Writer] implementation.
 type WTester struct {
 	w       io.Writer
 	muW     sync.Mutex // guards w
@@ -22,6 +26,9 @@ func NewWTester(w io.Writer) *WTester {
 	}
 }
 
+// Write writes the provided byte slice to the underlying
+// io.Writer and checks if the byte slice matches any of
+// the expectations set on the WTester.
 func (l *WTester) Write(p []byte) (n int, err error) {
 	for _, e := range l.expects {
 		ok := e.f(p)
@@ -43,6 +50,8 @@ func (l *WTester) Write(p []byte) (n int, err error) {
 	return l.w.Write(p)
 }
 
+// Close closes the underlying io.Writer if it implements
+// the [io.Closer] interface.
 func (l *WTester) Close() error {
 	if c, ok := l.w.(io.Closer); ok {
 		return c.Close()
@@ -50,6 +59,10 @@ func (l *WTester) Close() error {
 	return nil
 }
 
+// Expect sets an expectation on the WTester. The title
+// is used to identify the expectation and the f parameter
+// is a function that takes a byte slice and returns a boolean
+// indicating if the expectation is met.
 func (l *WTester) Expect(title string, f ExpectFunc) *Expect {
 	e := NewExpect(title, f)
 	l.expects[title] = e
@@ -57,6 +70,8 @@ func (l *WTester) Expect(title string, f ExpectFunc) *Expect {
 	return e
 }
 
+// Reset resets the WTester by clearing all expectations
+// and errors.
 func (l *WTester) Reset() {
 	l.muW.Lock()
 	defer l.muW.Unlock()
@@ -68,6 +83,8 @@ func (l *WTester) Reset() {
 	l.errors = make(map[string]*ExpectError)
 }
 
+// Validate validates the expectations set on the WTester
+// and returns an error if any of the expectations are not met.
 func (l *WTester) Validate() error {
 	for _, e := range l.expects {
 		switch {
@@ -94,6 +111,7 @@ func (l *WTester) Validate() error {
 	return ve
 }
 
+// appendError appends an error to the WTester's error map.
 func (l *WTester) appendError(title string, e ErrorRecord) {
 	l.muErr.Lock()
 	defer l.muErr.Unlock()
